c51ea0520d07a171866fb5187d8d3a10f46fd7ff
girishajk <167055236+girishajk@users.noreply.github.com>
2025-06-21 15:39:00 +0530

    Update FinancialForecast.java

--- week1/financial forecast/FinancialForecast.java
+++ week1/financial forecast/FinancialForecast.java
@@ -1,1 +1,16 @@
+public class FinancialForecast {
+    public static double calculateFutureValue(double presentValue, double rate, int years) {
+        if (years == 0) {
+            return presentValue;
+        }
+        return (1 + rate) * calculateFutureValue(presentValue, rate, years - 1);
+    }
 
+    public static void main(String[] args) {
+        double presentValue = 10000.0;
+        double annualRate = 0.05; 
+        int years = 10;
+        double futureValue = calculateFutureValue(presentValue, annualRate, years);
+        System.out.printf("Future Value after %d years: ₹%.2f\n", years, futureValue);
+    }
+}
