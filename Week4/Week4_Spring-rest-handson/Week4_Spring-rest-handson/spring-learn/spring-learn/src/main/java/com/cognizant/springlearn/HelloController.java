8cda39754dd66914d07d8bda80db3dbdede9a244
girishajk <167055236+girishajk@users.noreply.github.com>
2025-07-14 21:05:21 +0530

    Add files via upload

---
+++ Week4/Week4_Spring-rest-handson/Week4_Spring-rest-handson/spring-learn/spring-learn/src/main/java/com/cognizant/springlearn/HelloController.java
@@ -0,0 +1,14 @@
+
+package com.cognizant.springlearn;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "👋 Hello from Spring Boot!";
+    }
+}
